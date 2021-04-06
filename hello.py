e17deab8bb28b21d3c39ac12b9b9e4f4cd67ade2
rindianiawanda <78516376+rindianiawanda@users.noreply.github.com>
2021-04-06 21:23:37 +0700

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,4 @@
+print ("----------Biodata----------")
+print ("Nama   : Rindiani Awanda Putri")
+print ("Kelas  : Teknik Informatika 2018 A")
+print ("Kampus : Universitas Nahdlatul Ulama Sidoarjo")
